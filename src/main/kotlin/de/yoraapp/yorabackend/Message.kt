6b0ee6336c1717ef5ea0254f35787b205c91ac95
Annalena Frey <annalena-frey1@web.de>
2026-06-28 20:36:51 +0200

    layering + database

---
+++ src/main/kotlin/de/yoraapp/yorabackend/Message.kt
@@ -0,0 +1,7 @@
+package de.yoraapp.yorabackend
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("messages")
+data class Message(val text: String, @Id val id: String? = null)
--- src/main/kotlin/de/yoraapp/yorabackend/MessageController.kt
+++ src/main/kotlin/de/yoraapp/yorabackend/MessageController.kt
@@ -1,11 +1,19 @@
 package de.yoraapp.yorabackend
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class MessageController {
+class MessageController(private val messageService: MessageService) {
     @GetMapping("/")
-    fun index(@RequestParam("name") name: String) = "Hello, $name!"
+    fun index(@RequestParam name: String) = "Hello, $name!"
+
+    @GetMapping("/messages")
+    fun listMessages() = messageService.findMessages()
+
+    @PostMapping("/add_message")
+    fun addMessage(@RequestBody message: Message) = messageService.save(message)
 }
