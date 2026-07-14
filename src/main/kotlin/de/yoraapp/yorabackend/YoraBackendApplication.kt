e22b30a0f7f92c0950b22b50f5140badda93a13d
Annalena Frey <annalena-frey1@web.de>
2026-07-14 15:10:42 +0200

    S3 config

--- src/main/kotlin/de/yoraapp/yorabackend/YoraBackendApplication.kt
+++ src/main/kotlin/de/yoraapp/yorabackend/YoraBackendApplication.kt
@@ -1,9 +1,11 @@
 package de.yoraapp.yorabackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(GarageProperties::class)
 class YoraBackendApplication
 
 fun main(args: Array<String>) {
